9bd63366bd086727204ad2006cd80f5639274c8a
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-06-08 05:57:19 +0000

    LINK , PAGE ,LAYOUT2

--- first/src/app/course/page.jsx
+++ first/src/app/course/page.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import image from "@/images/download.jpg"   
+import Image from 'next/image';
 export default function page(){
     return (
         <main>
         <div>
+            {/* we can import the image from import image from "@/images/download.jpg" */}
             this is the page component --- this is the course page
+            <img src = {image.src}/>
+            <Image width={400} height ={400} src = {image.src}/>
         </div>
         </main>
+        //now we want to import image -- we can import it from the image folder
     )
 }
